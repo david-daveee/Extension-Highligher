681be1e33a97f04fc53b3b5529f3365b7edee2c0
David Nalivkin <84546888+ace-sparks@users.noreply.github.com>
2023-04-25 12:26:07 +0300

    First files (already works)
    1. First files that already working
    you can highlight from the code

---
+++ main.js
@@ -0,0 +1,94 @@
+window.addEventListener('load', function() {
+    'use strict';
+    var course_1 = 2;
+    var course_2 = 8;
+    var course_3 = 16;
+    var course_4 = 29;
+    var course_5 = 31;
+    var course_6 = 30;
+    setTimeout(() => {
+  let p = document.getElementsByClassName("nw-fm-tree-folder");
+        p[course_1].style.backgroundColor = "#80ff80";
+        p[course_2].style.backgroundColor = "#80ff80";
+        p[course_3].style.backgroundColor = "#80ff80";
+        p[course_4].style.backgroundColor = "#80ff80";
+        p[course_5].style.backgroundColor = "#80ff80";
+        p[course_6].style.backgroundColor = "#80ff80";
+
+ }, 5000);
+    setTimeout(() => {
+  let p = document.getElementsByClassName("nw-fm-tree-folder");
+        p[course_1].style.backgroundColor = "#80ff80";
+        p[course_2].style.backgroundColor = "#80ff80";
+        p[course_3].style.backgroundColor = "#80ff80";
+        p[course_4].style.backgroundColor = "#80ff80";
+        p[course_5].style.backgroundColor = "#80ff80";
+        p[course_6].style.backgroundColor = "#80ff80";
+
+ }, 6000);
+    setTimeout(() => {
+  let p = document.getElementsByClassName("nw-fm-tree-folder");
+        p[course_1].style.backgroundColor = "#80ff80";
+        p[course_2].style.backgroundColor = "#80ff80";
+        p[course_3].style.backgroundColor = "#80ff80";
+        p[course_4].style.backgroundColor = "#80ff80";
+        p[course_5].style.backgroundColor = "#80ff80";
+        p[course_6].style.backgroundColor = "#80ff80";
+
+ }, 7000);
+    setTimeout(() => {
+  let p = document.getElementsByClassName("nw-fm-tree-folder");
+        p[course_1].style.backgroundColor = "#80ff80";
+        p[course_2].style.backgroundColor = "#80ff80";
+        p[course_3].style.backgroundColor = "#80ff80";
+        p[course_4].style.backgroundColor = "#80ff80";
+        p[course_5].style.backgroundColor = "#80ff80";
+        p[course_6].style.backgroundColor = "#80ff80";
+
+ }, 8000);
+    setTimeout(() => {
+  let p = document.getElementsByClassName("nw-fm-tree-folder");
+        p[course_1].style.backgroundColor = "#80ff80";
+        p[course_2].style.backgroundColor = "#80ff80";
+        p[course_3].style.backgroundColor = "#80ff80";
+        p[course_4].style.backgroundColor = "#80ff80";
+        p[course_5].style.backgroundColor = "#80ff80";
+        p[course_6].style.backgroundColor = "#80ff80";
+
+ }, 9000);
+    setTimeout(() => {
+  let p = document.getElementsByClassName("nw-fm-tree-folder");
+        p[course_1].style.backgroundColor = "#80ff80";
+        p[course_2].style.backgroundColor = "#80ff80";
+        p[course_3].style.backgroundColor = "#80ff80";
+        p[course_4].style.backgroundColor = "#80ff80";
+        p[course_5].style.backgroundColor = "#80ff80";
+        p[course_6].style.backgroundColor = "#80ff80";
+
+ }, 10000);
+
+ setTimeout(() => {
+    let p = document.getElementsByClassName("nw-fm-tree-folder");
+          p[course_1].style.backgroundColor = "#80ff80";
+          p[course_2].style.backgroundColor = "#80ff80";
+          p[course_3].style.backgroundColor = "#80ff80";
+          p[course_4].style.backgroundColor = "#80ff80";
+          p[course_5].style.backgroundColor = "#80ff80";
+          p[course_6].style.backgroundColor = "#80ff80";
+  
+   }, 11000);
+
+   setTimeout(() => {
+    let p = document.getElementsByClassName("nw-fm-tree-folder");
+          p[course_1].style.backgroundColor = "#80ff80";
+          p[course_2].style.backgroundColor = "#80ff80";
+          p[course_3].style.backgroundColor = "#80ff80";
+          p[course_4].style.backgroundColor = "#80ff80";
+          p[course_5].style.backgroundColor = "#80ff80";
+          p[course_6].style.backgroundColor = "#80ff80";
+  
+   }, 12000);
+}
+
+
+);
